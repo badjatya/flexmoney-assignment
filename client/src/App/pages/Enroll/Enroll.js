36121295fd53f6ec2e3dc1e72dc9b66dcd558446
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-12-13 20:31:23 +0530

    [Feat]: Enrolling user success

--- client/src/App/pages/Enroll/Enroll.js
+++ client/src/App/pages/Enroll/Enroll.js
@@ -1,10 +1,11 @@
 // Importing Packages
 import React, { useState } from "react";
 import Select from "react-select";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // Importing Images
 import enrollImg from "../../assets/enroll.svg";
+import successImg from "../../assets/success.svg";
 
 // Importing Styles
 import "./Enroll.scss";
@@ -16,13 +17,27 @@ const Enroll = () => {
   const [batch, setBatch] = useState("");
   const [age, setAge] = useState("");
   const [error, setError] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
 
+  // Batch options
   const batchOptions = [
     { value: "6-7 AM", label: "6-7 AM" },
     { value: "7-8 AM", label: "7-8 AM" },
     { value: "8-9 AM", label: "8-9 AM" },
     { value: "6-6 PM", label: "6-6 PM" },
   ];
+
+  // Success Container
+  const Success = () => (
+    <div className="success">
+      <div>
+        <img src={successImg} alt="success" />
+        <Link to="/" className="btn-primary">
+          Home
+        </Link>
+      </div>
+    </div>
+  );
 
   // HandleSubmit
   const handleSubmit = async (e) => {
@@ -69,85 +84,96 @@ const Enroll = () => {
 
     // Verifying
     if (response.status === "success") {
-      console.log(response);
+      setName("");
+      setEmail("");
+      setAge("");
+      setBatch("");
+      setError("");
+      setShowSuccess(true);
     } else {
       setError(response.message);
     }
   };
 
   return (
-    <div className="Enroll">
-      <div className="container">
-        <div className="left">
-          <p className="heading">Enroll Yoga Class</p>
-          <p className="error">{error}</p>
-          <form onSubmit={handleSubmit}>
-            <div className="inputContainer">
-              <div className="inputCon">
-                <input
-                  type="text"
-                  placeholder="Enter Name"
-                  className="input"
-                  name="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className="inputCon">
-                <input
-                  type="email"
-                  placeholder="Enter Email"
-                  className="input"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
-            <div className="inputContainer">
-              <div className="inputCon">
-                <input
-                  type="number"
-                  className="input"
-                  placeholder="Enter Age"
-                  name="dateOfBirth"
-                  value={age}
-                  onChange={(e) => setAge(e.target.value)}
-                />
-              </div>
-              <div className="inputCon">
-                <Select
-                  className="select"
-                  placeholder="Select Batch"
-                  value={batch}
-                  onChange={setBatch}
-                  options={batchOptions}
-                  styles={{
-                    control: (styles) => ({
-                      ...styles,
-                      background: "#FFF",
-                      outline: "1px solid #333",
-                      borderRadius: "5px",
-                      width: "240px",
-                    }),
-                  }}
-                />
-              </div>
-            </div>
+    <>
+      {showSuccess ? (
+        <Success />
+      ) : (
+        <div className="Enroll">
+          <div className="container">
+            <div className="left">
+              <p className="heading">Enroll Yoga Class</p>
+              <p className="error">{error}</p>
+              <form onSubmit={handleSubmit}>
+                <div className="inputContainer">
+                  <div className="inputCon">
+                    <input
+                      type="text"
+                      placeholder="Enter Name"
+                      className="input"
+                      name="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="inputCon">
+                    <input
+                      type="email"
+                      placeholder="Enter Email"
+                      className="input"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="inputContainer">
+                  <div className="inputCon">
+                    <input
+                      type="number"
+                      className="input"
+                      placeholder="Enter Age"
+                      name="dateOfBirth"
+                      value={age}
+                      onChange={(e) => setAge(e.target.value)}
+                    />
+                  </div>
+                  <div className="inputCon">
+                    <Select
+                      className="select"
+                      placeholder="Select Batch"
+                      value={batch}
+                      onChange={setBatch}
+                      options={batchOptions}
+                      styles={{
+                        control: (styles) => ({
+                          ...styles,
+                          background: "#FFF",
+                          outline: "1px solid #333",
+                          borderRadius: "5px",
+                          width: "240px",
+                        }),
+                      }}
+                    />
+                  </div>
+                </div>
 
-            {/* Submit Button */}
-            <div className="inputContainer">
-              <button type="submit" className="btn">
-                Enroll
-              </button>
+                {/* Submit Button */}
+                <div className="inputContainer">
+                  <button type="submit" className="btn">
+                    Enroll
+                  </button>
+                </div>
+              </form>
             </div>
-          </form>
+            <div className="right">
+              <img src={enrollImg} alt="Enroll" />
+            </div>
+          </div>
         </div>
-        <div className="right">
-          <img src={enrollImg} alt="Enroll" />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
